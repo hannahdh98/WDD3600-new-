8c32c3f66c59dffa265a902f1ab4005bd5fd5aef
hannahdh98 <56090227+hannahdh98@users.noreply.github.com>
2021-03-02 10:31:19 -0500

    Update cart-item.js

--- models/cart-item.js
+++ models/cart-item.js
@@ -1,5 +1,7 @@
+//creat the sequelize instance to connect to the database
 const Sequelize = require('sequelize');
 
+//state where the database is and where to get it.
 const sequelize = require('../util/database');
 
 const CartItem = sequelize.define('cartItem', {
@@ -9,6 +11,7 @@ const CartItem = sequelize.define('cartItem', {
     allowNull: false,
     primaryKey: true
   },
+  //makes the sequelize data type integer
   quantity: Sequelize.INTEGER
 });
 module.exports = CartItem;
