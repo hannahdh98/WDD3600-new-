7685bf4e046e64b69040b58d196cf755e65d7bc9
hannahdh98 <56090227+hannahdh98@users.noreply.github.com>
2021-03-03 12:27:33 -0500

    Update order.js

--- models/order.js
+++ models/order.js
@@ -1,5 +1,6 @@
+//creat the sequelize instance to connect to the database
 const Sequelize = require('sequelize');
-
+//state where the database is and where to get it.
 const sequelize = require('../util/database');
 
 const Order = sequelize.define('order', {
