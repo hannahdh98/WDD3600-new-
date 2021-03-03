2f2cd1c65f67d25ea3e40bbb80164ca54d71a987
hannahdh98 <56090227+hannahdh98@users.noreply.github.com>
2021-03-03 12:32:31 -0500

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,9 +1,11 @@
+//creat the sequelize instance to connect to the database
 const Sequelize = require('sequelize');
-
+//state where the database is and where to get it/path
 const sequelize = require('../util/database');
-
+//adds columns
 const User = sequelize.define('user', {
   id: {
+    //adds comments
     type: Sequelize.INTEGER,
     autoIncrement: true,
     allowNull: false,
@@ -12,5 +14,5 @@ const User = sequelize.define('user', {
   name: Sequelize.STRING,
   email: Sequelize.STRING
 });
-
+//exports users table
 module.exports = User;
