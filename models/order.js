e92224abc767a982b77d74b28ebb4b5e33b88e5f
hannahdh98 <56090227+hannahdh98@users.noreply.github.com>
2021-03-03 12:29:27 -0500

    Update order.js

--- models/order.js
+++ models/order.js
@@ -2,10 +2,11 @@
 const Sequelize = require('sequelize');
 //state where the database is and where to get it.
 const sequelize = require('../util/database');
-
+// Adding columns
 const Order = sequelize.define('order', {
   id: {
     type: Sequelize.INTEGER,
+    // create auto_incrementing integer columns
     autoIncrement: true,
     allowNull: false,
     primaryKey: true
