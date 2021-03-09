740a393b8350e0a126a9d6e42b373697544e4305
hannahdh98 <56090227+hannahdh98@users.noreply.github.com>
2021-03-09 11:34:51 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,49 @@
+//import path
 const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
 
+//imports error.js
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
 
+//imports modules
 const app = express();
-
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+//has the bodyParser, pardt the body that is sent throhght the form
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// app.use ((req, res, next) =>
+//    User.findbyId()
+//    .then
+// });
 app.use((req, res, next) => {
-  User.findById('5baa2528563f16379fc8a610')
+  //uses id to search for that user
+  User.findById('')//my mongo compass wont connect so I am not able to get this id.
     .then(user => {
       req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
 });
-
+//router object
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
+//middleware to handle 404
+//app.use((req, res, next) => {
+      // res.status(404).render('404', {pageTitle: 'Page Not Found });
+//});
+
+//get404 function
 app.use(errorController.get404);
 
 mongoConnect(() => {
